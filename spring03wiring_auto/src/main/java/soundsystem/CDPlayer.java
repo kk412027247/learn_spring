0b86a4a866b4a576ccfcc7f943ae9604b0bbf6ec
TMD <kk412027247@gmail.com>
2020-08-11 01:14:20 +0800

    unit  @Autowired

--- spring03wiring_auto/src/main/java/soundsystem/CDPlayer.java
+++ spring03wiring_auto/src/main/java/soundsystem/CDPlayer.java
@@ -6,20 +6,49 @@ import org.springframework.stereotype.Component;
 @Component
 public class CDPlayer {
 
+    //    @Autowired
     private CompactDisc cd;
+
+    //    @Autowired
+    private Power power;
 
     public CDPlayer() {
         super();
         System.out.println("CDPlayer no parameter");
     }
 
+//    @Autowired
+//    public void setCd(CompactDisc cd) {
+//        this.cd = cd;
+//    }
+//
+//    @Autowired
+//    public void setPower(Power power) {
+//        this.power = power;
+//    }
+//
+//
+//    @Autowired
+//    public CDPlayer(CompactDisc cd) {
+//        this.cd = cd;
+//        System.out.println("CDPlayer constructor with parameter");
+//    }
+
     @Autowired
-    public CDPlayer(CompactDisc cd) {
+    public CDPlayer(CompactDisc cd, Power power) {
         this.cd = cd;
-        System.out.println("CDPlayer constructor with parameter");
+        this.power = power;
+        System.out.println("CDPlayer constructor with CompactDisc and Power");
     }
 
-    public void play(){
+//    @Autowired
+//    public void prepare(CompactDisc cd, Power power) {
+//        this.cd = cd;
+//        this.power = power;
+//    }
+
+    public void play() {
+        power.supply();
         cd.play();
     }
 }
