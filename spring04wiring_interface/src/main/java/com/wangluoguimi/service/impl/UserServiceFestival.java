9b13cb7b297aa8932e8a127244928e3e20b84603
TMD <kk412027247@gmail.com>
2020-08-11 23:11:39 +0800

    @Qualifier

--- spring04wiring_interface/src/main/java/com/wangluoguimi/service/impl/UserServiceFestival.java
+++ spring04wiring_interface/src/main/java/com/wangluoguimi/service/impl/UserServiceFestival.java
@@ -2,14 +2,16 @@ package com.wangluoguimi.service.impl;
 
 import com.wangluoguimi.service.UserService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class UserServiceNormal implements UserService {
+@Qualifier("festival")
+//@Primary
+public class UserServiceFestival implements UserService {
     @Override
-    @Qualifier("normal")
     public void add() {
-        System.out.println("add user");
+        System.out.println("send coupon to user");
     }
 }
--- spring04wiring_interface/src/test/java/com/wangluoguimi/service/UserServiceTest.java
+++ spring04wiring_interface/src/test/java/com/wangluoguimi/service/UserServiceTest.java
@@ -1,15 +1,19 @@
 package com.wangluoguimi.service;
 
+import com.wangluoguimi.service.impl.UserServiceNormal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class UserServiceTest {
+
     @Autowired
+    @Qualifier("festival")
     private UserService userService;
 
     @Test
